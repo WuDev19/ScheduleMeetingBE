5bbd33f6d2dd49eac82830592f001237c03fddae
Wu <nguyenvu19a19@gmail.com>
2026-05-28 19:38:36 +0700

    feat: init project

---
+++ src/test/java/com/example/schedulemeetingbe/ScheduleMeetingBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.schedulemeetingbe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScheduleMeetingBeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
