d6ff3e555d3416790d1f4856b0ab5cd2e4780d94
Wu <nguyenvu19a19@gmail.com>
2026-06-03 21:18:21 +0700

    feat: add user controller

--- src/main/java/com/example/schedulemeetingbe/controller/UserController.java
+++ src/main/java/com/example/schedulemeetingbe/controller/UserController.java
@@ -1,13 +1,49 @@
 package com.example.schedulemeetingbe.controller;
 
+import com.example.schedulemeetingbe.constant.Constants;
+import com.example.schedulemeetingbe.constant.StringCommon;
+import com.example.schedulemeetingbe.dto.common.ApiResponse;
+import com.example.schedulemeetingbe.dto.request.CreateUserRequest;
+import com.example.schedulemeetingbe.service.base.IUserService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.parameters.RequestBody;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import io.swagger.v3.oas.annotations.tags.Tag;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping
+@RequestMapping("/api/v1/user")
 @Tag(name = "Api CRUD User")
+@RequiredArgsConstructor
 public class UserController {
 
+    private final IUserService iUserService;
 
+    @SecurityRequirement(name = StringCommon.SECURITY_SCHEME)
+    @Operation(summary = "Api lấy thông tin chi tiết người dùng")
+    @GetMapping("/me/{id}")
+    @PreAuthorize("hasAuthority('USER:VIEW')")
+    public ResponseEntity<?> getDetailUser(@PathVariable Long id) {
+        return ApiResponse.success(
+                iUserService.getUserDetail(id),
+                "Lấy thông tin chi tiết taì khoản thành công",
+                Constants.SUCCESS_CODE
+        );
+    }
+
+    @SecurityRequirement(name = StringCommon.SECURITY_SCHEME)
+    @Operation(summary = "Api cho admin tạo tài khoản người dùng")
+    @PostMapping
+    @PreAuthorize("hasAuthority('USER:CREATE')")
+    public ResponseEntity<?> createAccount(@Valid @RequestBody CreateUserRequest request){
+        return ApiResponse.success(
+                iUserService.createUser(request),
+                "Tạo tài khoản cho người dùng thành công",
+                Constants.SUCCESS_CODE
+        );
+    }
 }
