e602c13b846cb9623414bc4d33ad3705881e32ec
Wu <nguyenvu19a19@gmail.com>
2026-06-11 22:57:45 +0700

    feat(feature/room): add search room via equipment

--- src/main/java/com/example/schedulemeetingbe/repository/RoomRepository.java
+++ src/main/java/com/example/schedulemeetingbe/repository/RoomRepository.java
@@ -5,10 +5,19 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface RoomRepository extends JpaRepository<Room, Long>, JpaSpecificationExecutor<Room> {
 
     Page<Room> findByIsActiveIsTrue(Pageable pageable);
 
-    Page<Room> findByRoomNameContainingIgnoreCase(String keyword, Pageable pageable);
+    @Query("""
+            SELECT DISTINCT re.room
+            FROM RoomEquipment re
+            WHERE (LOWER(re.room.roomName) LIKE LOWER(CONCAT('%', :keyword, '%'))
+                    OR LOWER(re.equipment.equipmentName) LIKE LOWER(CONCAT('%', :keyword, '%')))
+                                AND re.room.deletedAt IS NULL
+            """)
+    Page<Room> searchRoom(@Param("keyword") String keyword, Pageable pageable);
 }
